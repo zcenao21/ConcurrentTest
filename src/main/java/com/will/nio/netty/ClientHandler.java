b57548dd2c2b16054a38698bb2f8aad9bb741238
will <zcenao21@gmail.com>
2025-08-23 15:50:47 +0800

    netty first test

---
+++ src/main/java/com/will/nio/netty/ClientHandler.java
@@ -0,0 +1,33 @@
+package com.will.nio.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.nio.charset.Charset;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("客户端发送消息");
+		ByteBuf buf = getByteBuf(ctx);
+		ctx.channel().writeAndFlush(buf);
+	}
+
+	private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+		ByteBuf buffer = ctx.alloc().buffer();
+		String sendMsg = "我是客户端1，您好";
+		byte[] bytes = sendMsg.getBytes(Charset.forName("utf-8"));
+		buffer.writeBytes(bytes);
+		return buffer;
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf = (ByteBuf) msg;
+		System.out.println(buf.toString(Charset.forName("utf-8")));
+	}
+
+}
--- src/main/java/com/will/nio/netty/NettyServer.java
+++ src/main/java/com/will/nio/netty/NettyServer.java
@@ -1,4 +1,25 @@
 package com.will.nio.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class NettyServer {
+	public static void main(String[] args) {
+		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+		NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+		ServerBootstrap serverBootstrap = new ServerBootstrap();
+		serverBootstrap
+				.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<NioSocketChannel>() {
+					@Override
+					protected void initChannel(NioSocketChannel channel) throws Exception {
+						channel.pipeline().addLast(new ServerHandler());
+					}
+				});
+		serverBootstrap.bind(2121);
+	}
 }
